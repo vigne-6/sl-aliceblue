7f56e9d4c19a574f81fbc76e55f63d649160ebeb
illyasviell <ashja234@gmail.com>
2020-06-05 11:36:55 +0700

    latency monitoring, clear

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,62 @@
 import * as fastify from "fastify"
 import fetch from 'node-fetch'
 import fs from 'fs'
+const server: fastify.FastifyInstance = fastify.default({ logger: true })
+const servers: Array<string> = [
+    "http://slimeserver.herokuapp.com",
+    "http://sl-aliceblue.herokuapp.com",
+    "http://sl-azure.herokuapp.com",
+    "http://sl-crimson.herokuapp.com",
+    "http://sl-firebrick.herokuapp.com",
+    "http://sl-magenta.herokuapp.com",
+    "https://sl-indigo.glitch.me",
+    "https://sl-ivory.glitch.me",
+    "https://sl-linen.glitch.me",
+    "https://sl-navy.glitch.me",
+    "https://sl-sienna.glitch.me",
+    "https://sl-slateblue.glitch.me",
+    "https://sl-thistle.glitch.me"
+]
+let latency: any = {}
+let users: Object = {}
 
-const server:fastify.FastifyInstance = fastify.default({logger:true})
+setInterval(
+    () => {
+        servers.forEach(async server => {
+            const b4fetch: number = new Date().getTime()
+            fetch(server).then(
+                ()=>{
+                    const l: number = new Date().getTime() - b4fetch
+                    latency[server] = l / 2
+                }
+            )
+        });
+    },
+    1000
+)
 
-server.get("/", async(req, res) => {
-    const stream = fs.createReadStream('./view/index.html')
-    res.type('text/html').send(stream)
-})
-const PORT:any = process.env.PORT || "3000"
+
+server.get(
+    "/",
+    async (req, res) => {
+        const stream = fs.createReadStream('./view/index.html')
+        res.type('text/html').send(stream)
+    }
+)
+
+server.get(
+    "/route",
+    async (req, res) => {
+
+    }
+)
+server.get(
+    "/latency",
+    async (req, res) => {
+        res.send(JSON.stringify(latency))
+    }
+)
+const PORT: any = process.env.PORT || "5000"
 
 server.listen(
     PORT,
